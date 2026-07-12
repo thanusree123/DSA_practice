6c3404f83634da397413b4197d52241ec2f3f009
thanusree123 <gollathanusree0@gmail.com>
2026-07-12 08:12:37 +0530

    py basics

--- grandtest4/arrays/leader.java
+++ grandtest4/arrays/leader.java
@@ -3,7 +3,6 @@ class leader{
     public static void main(String args[]){
         Scanner s=new Scanner(System.in);
         int n=s.nextInt();
-        int sum=0;
         int arr[]=new int[n];
         for(int i=0;i<n;i++){
             arr[i]=s.nextInt();
---
+++ python/basic.py
@@ -0,0 +1,14 @@
+
+# x=10
+# print(type(x))
+# print(float(x))
+# name=input("enter your name ")
+# print(name) 
+# name = "Thanu"
+# age = 21
+# print(f"name {name} age {age}")
+x = int(input("Enter: "))
+y = float(input("Enter: "))
+
+print(type(x))
+print(type(y))
