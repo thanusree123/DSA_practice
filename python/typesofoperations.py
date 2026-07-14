180deff0e1932cea60df0d872aa22e8c2efffe4c
thanusree123 <gollathanusree0@gmail.com>
2026-07-14 21:16:33 +0530

    python operations

---
+++ python/typesofoperations.py
@@ -0,0 +1,30 @@
+# a = 10
+# b = 20
+# print(a + b)
+
+# print(20 - 8)
+
+# print(6 * 4)
+
+# print(10 / 2)
+
+# float division
+# print(10 // 3)
+
+# print(10 % 3)
+
+# if 8 % 2 == 0:
+#     print("Even")
+
+# exponential
+# print(2 ** 3) 
+
+x = 10
+# x += 5
+# x-=3
+# x*=2
+# x/=2
+# x//=2
+# x%=2
+x**=2
+print(x)
