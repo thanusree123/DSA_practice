251cca9e8d21b216d43f66564fdf210be76933b8
thanusree123 <gollathanusree0@gmail.com>
2026-07-18 11:25:36 +0530

    python

--- python/typesofoperations.py
+++ python/typesofoperations.py
@@ -26,5 +26,64 @@ x = 10
 # x/=2
 # x//=2
 # x%=2
-x**=2
-print(x)
+# x**=2
+# print(x)x
+
+# and operator 
+# age=20
+# has_id=True
+# print(age>18 and has_id)
+
+# age=16
+# has_id=True
+# print(age>18 and has_id)
+
+# in operation
+# name="Python"
+# print("P" in name)
+
+# languages={"Python","C++","Java"}
+# print("Python" in languages)
+
+# if True:
+#     print("Hello")
+
+# age = 16
+
+# if age >= 18:
+#     print("Allowed")
+# else:
+#     print("Not Allowed")
+
+
+# marks = 95
+
+# if marks >= 90:
+#     print("A Grade")
+
+# elif marks >= 75:
+#     print("B Grade")
+
+# elif marks >= 50:
+#     print("C Grade")
+
+# else:
+#     print("Fail")
+
+#ternary operation 
+"""age = 20
+if age >= 18:
+    status = "Adult"
+else:
+    status = "Minor"
+    
+    status="Adult" if age>=18 else "Minor"
+print(status)"""
+
+# while:
+i=1
+while i<=5:
+    print(i)
+    i+1
+    
+    
